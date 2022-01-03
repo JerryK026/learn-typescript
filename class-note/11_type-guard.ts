8a33bbe08f8153eb5c29c998204c1264713ae91e
SeokyungKim <kskyung0624@gmail.com>
2022-01-03 14:52:14 +0900

    타입 가드

---
+++ class-note/11_type-guard.ts
@@ -0,0 +1,36 @@
+interface Developer {
+  name: string;
+  skill: string;
+}
+
+interface Person {
+  name: string;
+  age: number;
+}
+
+function introduce(): Developer | Person {
+  return { name: 'Tony', age: 33, skill: 'Iron Making' }
+}
+
+var tony = introduce();
+// console.log(tony.skill);
+
+// 타입 가드 없이 타입 단언만으로 추론할 수 있도록 줄여나가기
+if ((tony as Developer).skill) {
+  var skill = (tony as Developer).skill;
+  console.log(skill);
+} else if ((tony as Person).age) {
+  var age = (tony as Person).age;
+  console.log(age);
+}
+
+// 타입 가드 정의
+function isDeveloper(target: Developer | Person): target is Developer {
+  return (target as Developer).skill !== undefined;
+}
+
+if (isDeveloper(tony)) {
+  console.log(tony.skill);
+} else {
+  console.log(tony.age);
+}
